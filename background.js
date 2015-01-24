af193c2a2b0b93c3ed3f7d8878ff8b48d41e4401
Vaidas <shibalba1@gmail.com>
2015-01-25 00:32:25 +0200

    eina per visus su taimeriu

--- background.js
+++ background.js
@@ -8,15 +8,17 @@ $(document).ready(function() {
             };
         });
     }
-    function add_yify_links(movie_id) {
+    function add_yify_links(movie_selected) {
+        //console.log("https://yts.re/api/listimdb.json?imdb_id=" + movie_selected.movie_id);return;
         $.ajax({
             type: "GET",
-            url: "https://yts.re/api/listimdb.json?imdb_id=" + movie_id
-        }).done(function (json) {
-            add_yify_link(json);
-        });
+            url: "https://yts.re/api/listimdb.json?imdb_id=" + movie_selected.movie_id
+        }).done(function (json_yify) {
+            add_yify_link(movie_selected, json_yify);
+            setInterval(function(){  }, 200);
+        }).fail(function(){}).error(function(){});
     }
-    function add_yify_link(json_yify) {
+    function add_yify_link(movie_selected, json_yify) {
         if (json_yify.MovieCount > 0) {
             var list_append_el = $('.list.detail > .list_item')
                 .eq(movie_selected.number_in_list).children('.info').first().children('.rating').first();
@@ -26,12 +28,12 @@ $(document).ready(function() {
             });
 
             var links_string = "<div class='secondary'><span>Qualities available for download (magnet link): </span>" + links.join(' ') + "</div>";
-
             list_append_el.after(links_string);
         }
     }
 
     var grabbed_movie_ids = grab_movie_ids();
-    var movie_selected = grabbed_movie_ids[0];
-    add_yify_links(movie_selected.movie_id);
+    $.each(grabbed_movie_ids, function(){
+        add_yify_links(this);
+    });
 });
