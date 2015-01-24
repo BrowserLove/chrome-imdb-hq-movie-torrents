4623c8c893d20bc7a200ca55da837027ce3f3e8f
Vaidas <shibalba1@gmail.com>
2015-01-25 00:14:56 +0200

    perdeliojam, pasalinam komentus

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    //console.log('Loaded');
-
     function grab_movie_ids() {
         return $(".list.detail > .list_item > .image > a").map(function (n) {
             return {
@@ -10,15 +8,7 @@ $(document).ready(function() {
             };
         });
     }
-    var grabbed_movie_ids = grab_movie_ids();
-//console.log(grabbed_movie_ids);
-//console.log(grabbed_imdb_movie_ids.length);
-
-    var movie_selected = grabbed_movie_ids[0];
-
     function add_yify_links(movie_id) {
-        //console.log("https://yts.re/api/listimdb.json?imdb_id=" + movie_id);
-
         $.ajax({
             type: "GET",
             url: "https://yts.re/api/listimdb.json?imdb_id=" + movie_id
@@ -26,15 +16,10 @@ $(document).ready(function() {
             add_yify_link(json);
         });
     }
-    add_yify_links(movie_selected.movie_id);
-
     function add_yify_link(json_yify) {
         if (json_yify.MovieCount > 0) {
-            //console.log(json_yify);
-
             var list_append_el = $('.list.detail > .list_item')
                 .eq(movie_selected.number_in_list).children('.info').first().children('.rating').first();
-            //console.log(list_append_el);
 
             var links = $.map(json_yify.MovieList, function(movie){
                 return '<a href="' + movie.TorrentMagnetUrl + '">' + movie.Quality + '</a>';
@@ -46,5 +31,7 @@ $(document).ready(function() {
         }
     }
 
-    //console.log('Done');
+    var grabbed_movie_ids = grab_movie_ids();
+    var movie_selected = grabbed_movie_ids[0];
+    add_yify_links(movie_selected.movie_id);
 });
