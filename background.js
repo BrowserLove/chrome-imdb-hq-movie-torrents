88f416bff5d884a585c0a32908e5a931a7ef3d14
Vaidas <shibalba1@gmail.com>
2015-01-25 13:55:56 +0200

    veikia ant list ir title page type

--- background.js
+++ background.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
             }
         });
     }
+    
     function append_yify_links(movie_selected, json_yify) {
         var links = $.map(json_yify.MovieList, function(movie){
             return '<a href="' + movie.TorrentMagnetUrl + '">' + movie.Quality + '</a>';
@@ -20,28 +21,68 @@ $(document).ready(function() {
         movie_selected.append_after.after(links_string);
     }
 
-    var ListItem = function(list_element){
-        this.listElement = list_element;
-        this.index = this.listElement.index();
+    function get_type(){
+        var pathname = window.location.pathname;
+
+        if(pathname.match(/list\//) !== null) {
+            return 'list';
+        }
+        else if(pathname.match(/title\//) !== null){
+            return 'title'
+        }
+
+        return '';
+    }
+    var page_type = get_type();
+
+    function get_content_element(page_type){
+        switch(page_type){
+            case 'list':
+                return $('div.list.detail div.list_item');
+            case 'title':
+                return $('div#title-overview-widget');
+        }
+    }
+
+    var MovieItem = function(element, page_type){
+        this.element = element;
+        this.index = this.element.index();
+        this.pageType = page_type;
 
         this.getMovieName = function(){
-            return this.listElement.children('div.info').children('b').children('a').text();
-        }
+            switch(this.pageType){
+                case 'list':
+                    return this.element.find('div.info > b > a').text();
+                case 'title':
+                    return this.element.find('h1.header > span[itemprop="name"]').text();
+            }
+        };
         this.getMovieId = function(){
-            return this.listElement.children('.image').children('a').children('div').attr('data-const');
-        }
+            switch(this.pageType){
+                case 'list':
+                    return this.element.find('.image > a > div').attr('data-const');
+                case 'title':
+                    return this.element.find('div.wlb_classic_wrapper a.wlb_watchlist_btn').attr('data-tconst');
+            }
+        };
         this.getAppendAfter = function(){
-            return this.listElement.children('.info').first().children('.rating');
-        }
+            switch(this.pageType){
+                case 'list':
+                    return this.element.find('.info > .rating');
+                case 'title':
+                    return this.element.find('div.star-box-details');
+            }
+        };
 
         this.name = this.getMovieName();
         this.id = this.getMovieId();
         this.append_after = this.getAppendAfter();
-    }
+    };
 
-    $('div.list.detail div.list_item').one('inview', function(e, isInView){
+    get_content_element(page_type).one('inview', function(e, isInView){
         if(isInView) {
-            add_yify_links(new ListItem($(this)));
+            //console.log(new MovieItem($(this), page_type));
+            add_yify_links(new MovieItem($(this), page_type));
         }
     });
 });
