62dec7036a0126e7e71be9cbb00ad41a2cce72e5
Vaidas Bagdonas <shibalba1@gmail.com>
2017-02-05 11:33:41 +0100

    refactored

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-## jQuery YIFY Movie Checker
+## IMDb Torrents
 ###Google Chrome extention
 
-####DEPRECATED
 IMDb now restricts foreign origin requests, therefore this extension is no longer usable.
 
-Lightweight and fast solution for adding links of available [YTS.AG](http://yts.ag/) aka YIFY torrent movie downloads (720p, 1080p, etc.) to IMDb itself.
+Lightweight and fast solution for adding links of available [YTS.AG](http://yts.ag/) A.K.A. YIFY torrent movie downloads (720p, 1080p, etc.) to IMDb itself.
 
-More information about the extention in the [Google Chrome Store](https://goo.gl/qMhU3c)
+More information about the extension in the [Google Chrome Store](https://goo.gl/qMhU3c)
--- app.js
+++ app.js
@@ -1,59 +1,68 @@
 (function(){
   var app = {
-    yts_api_url: "https://yts.ag/api/v2/",
+    yts_url: "https://yts.ag/",
+    movieTitle: '',
 
-    movie: {
-      id: '',
-      magnet: ''
-    },
-
-    init: function(movieId){
-      this.movie.id = movieId;
-
-      if(this.movie.id == '' || this.movie.id == undefined){
-        console.error('Movie ID not set');
-      }
-
+    init: function(movieTitle){
+      this.movieTitle = movieTitle;
       this.cacheDom();
     },
+
     cacheDom: function(){
-      this.movieDownloadBlock = $("<div class='credit_summary_item'><h4 class='inline'>Torrent:</h4></div>");
+      this.movieDownloadBlock = $("<div class='credit_summary_item'><h4 class='inline'>Torrents:</h4></div>");
 
       this.spinner = $("<div class='ajax_spin'></div>");
       this.movieDownloadBlock.append(this.spinner);
 
-      this.movieDownloadLinks = $("<ul></ul>");
+      this.movieDownloadLinks = $("<ul class='torrents'></ul>");
       this.movieDownloadBlock.append(this.movieDownloadLinks);
 
       $('.credit_summary_item').last().after(this.movieDownloadBlock);
     },
-    fetchMovieMagnetLink(){
-      axios({
-        method: 'get',
-        url: this.yts_api_url + "movie_details.jsonp?movie_id=" + this.movie.id
-      })
-      .then(function (response) {
-        console.log(response);
 
-        this.render();
-      }.bind(this))
-      .catch(function (error) {
+    appendTorrentLink: function(link, quality, type, isLast){
+      this.movieDownloadLinks.append(
+        "<li><a href='" + link + "'>" + quality + " " + type + "</a>" + (!isLast ? ',' : '') + "</li>"
+      );
+    },
+
+    fetchTorrentMagnetLinks: function(){
+      var self = this;
+
+      axios.get(self.yts_url + "ajax/search", { params: {
+          query: self.movieTitle
+      }}).then(function (response) {
+        if(response.data.data && response.data.data[0]){
+          axios.get(response.data.data[0].url).then(function (response) {
+
+            var links = $(response.data).find('.modal-download a');
+            links.each(function(i, link){
+              var link = $(link);
+              var quality = link.attr('title').split(' ');
+
+          		self.appendTorrentLink(
+                link.attr('href'),
+                quality[quality.length - 2],
+                link.text().toLowerCase() == 'download' ? 'torrent' : 'magnet',
+                links.length - 1 == i
+              );
+          	});
+
+            self.spinner.hide();
+          }).catch(function (error) {
+            console.log(error);
+          });
+        }
+      }).catch(function (error) {
         console.log(error);
       });
-    },
-    render: function(){
-      if(this.movie.magnet != ''){
-        //
-
-        this.spinner.hide();
-      }
     }
   }
 
   $(document).ready(function(){
-    var movieId = location.href.match(/\/tt([0-9]+)\//)[1];
+    var movieTitle = $('h1').text().split('(')[0].trim();
 
-    app.init(movieId);
-    app.fetchMovieMagnetLink();
+    app.init(movieTitle);
+    app.fetchTorrentMagnetLinks();
   });
 })();
